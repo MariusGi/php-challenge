6b171383ec6dad581d23f041a6dd22c9f4422725
MariusGi <mariusgirnis@gmail.com>
2020-11-14 16:37:38 +0200

    add getResponse method and responds method code in Jurgis class

--- src/Jurgis.php
+++ src/Jurgis.php
@@ -1,13 +1,26 @@
 <?php
 
-
 namespace App;
-
 
 class Jurgis
 {
     public function responds(string $message): string
     {
-        //TODO: your code
+        return $this->getResponse($message);
+    }
+
+    private function getResponse(string $message): string
+    {
+        if (strpos($message, '?') !== false) {
+            $response = 'Okis.';
+        } elseif (strpos($message, '!') !== false) {
+            $response = 'Oi oi, atvėsk!';
+        } elseif (!$message) {
+            $response = 'Alio! Kuku?';
+        } else {
+            $response = 'Aha gerai.';
+        }
+
+        return $response;
     }
 }
